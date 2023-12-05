e07b3c89b4ca631dd9c446a3804166543bfb37ed
tarikyesilcimenn <tarikyesilcimenn@gmail.com>
2023-12-05 19:17:48 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+
+
+
+</script>
+
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView /> -->
+  <div class="loginComponent">
+
+  </div>
+</template>
+
+<style scoped>
+
+.loginComponent {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    data(){
+        
+    }
+};
+</script>
+<template>
+  <div class="box">
+    <label>Email</label>
+    <input />
+    <label>Şifre</label>
+    <input />
+    <label>Şifre Tekrarı</label>
+    <input />
+  </div>
+</template>
+
+<style scoped>
+.box {
+  display: flex;
+  padding: 60px 60px;
+  border-radius: 12px;
+  background-color: cornflowerblue;
+  flex-direction: column;
+  width: 200px;
+}
+.box label {
+  color: black;
+  font-size: 18px;
+  margin-bottom: 5px;
+
+}
+.box input {
+  margin-bottom: 5px;
+  height: 30px;
+  border-radius: 6px;
+  border: 0;
+}
+</style>
